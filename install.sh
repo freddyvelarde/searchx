b33f3e06292b4d709a2c40fb06165f0a53ef7d30
freddyvelarde <freddyvelarde59@gmail.com>
2023-08-03 18:30:50 -0400

    `install.sh` file improved

--- install.sh
+++ install.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-echo 'Creating directory'
-mkdir ~/test-hello-world
+echo 'Cloning directory'
+
+git clone https://github.com/freddyvelarde/searchx.git ~/searchx
+
+cd ~/searchx
+
+sudo cp -rf ./searchx /usr/bin
+
+if [ "$SHELL" = "/bin/bash" ]; then
+  source ~/.bashrc
+elif [ "$SHELL" = "/usr/bin/zsh" ]; then
+  source ~/.zshrc
+elif [ "$fish" = "/usr/bin/fish" ]; then
+  source ~/.config/fish/config.fish
+fi
+
+rm -rf ~/searchx
+
+echo 'Installation finished'
